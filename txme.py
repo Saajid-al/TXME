9908d5f9b7d1650dfffe91cdce899640cbc51ed6
Saaji <saajid.aliyar@outlook.com>
2024-11-25 09:27:06 -0500

    first commit

---
+++ txme.py
@@ -0,0 +1,4 @@
+#personal texting assistant app. Would be able to message it, and it’ll schedule texts for you. 
+#For example, remind me interview at 5am. can schedule txts for morning and so on. 
+#Later development would be able to subscribe to anything and have it sent to your phone. 
+#IE being able to get a message that the new chapter for your manga drop. (your own personal newsletter)
